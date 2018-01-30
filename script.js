ad02078898b860173fc40b228a18d84bc1dc79c1
Chance Smith <chancesmithb@gmail.com>
2018-01-30 07:43:50 -0600

    adds ext files

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // Load the function after DOM ready.
+
+  // // absolute path of the file residing your extension.
+  // var link1=chrome.extension.getURL("img/ClockifyExtLogo.png");
+
+  // unbind the default click action preventer!!!!
+  $(".sidebar__menu--list ul li").unbind("click");
+});
